b2627954a3e37040547278a2bee292f83074cece
Souparna Nath <56977239+SOUPARNA-NATH@users.noreply.github.com>
2019-11-06 16:49:25 +0530

    100 Fibonacci Number Generator

---
+++ Fibonacci_100_TIFR.py
@@ -0,0 +1,12 @@
+"""
+This is a program to import a self created module for generating n Fibonacci 
+Numbers and to use it to generate 100 Fibonacci Numbers
+
+Author:- Souparna Nath
+
+Date:- 06.11.2019
+"""
+
+import Fiibonacci_TIFR as fib
+
+print(fib.FibonacciGenerate(20))
