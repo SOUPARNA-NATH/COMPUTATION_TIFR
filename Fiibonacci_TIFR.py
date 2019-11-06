ddd5fdc0e76b6fca2f299188d99c5d7b1d85bf3b
Souparna Nath <56977239+SOUPARNA-NATH@users.noreply.github.com>
2019-11-06 16:56:13 +0530

    Modified Fiibonaci_TIFR.py
    This now prints n Fibonacci Numbers provided by the user

--- Fiibonacci_TIFR.py
+++ Fiibonacci_TIFR.py
@@ -1,19 +1,22 @@
 """
-This is a program to print the first 10 fibonacci numbers
+This is a program to generate n Fibonacci Numbers
 
 Author:- Souparna Nath
 
 Date:- 05.11.2019
 """
 
-first = 0
-second = 1
-
-print (first)
-print (second)
-
-for i in range(10):
-    third = first + second
-    print (third)
-    first = second
-    second = third
+def FibonacciGenerate(n):
+    
+    first = 0
+    second = 1
+    
+    Fib = []
+    Fib.extend([first,second])
+    
+    for i in range(n-2):
+        third = first + second
+        Fib.append(third)
+        first = second
+        second = third
+    return Fib
