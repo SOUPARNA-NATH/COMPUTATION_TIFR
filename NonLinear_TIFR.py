5b9be64ddf59ec56650966e3f918694f4eccde36
Souparna Nath <56977239+SOUPARNA-NATH@users.noreply.github.com>
2019-12-09 01:31:43 +0530

    Now Uses Secant Method
    Basically scipy.optimize.newton() uses secant method if the derivative is not provided

--- NonLinear_TIFR.py
+++ NonLinear_TIFR.py
@@ -10,16 +10,9 @@ import numpy as np
 def f(x):
     return np.sin(np.cos(np.exp(x)))
 
-def fprime(x):
-    return -np.exp(x)*np.sin(np.exp(x))*np.cos(np.cos(np.exp(x)))
-
-root = opt.newton(f,-0.1,fprime)
+root = opt.newton(f,-0.1)
 val = f(root)
 
 print("The root of sin(cos(exp(x))) is ",root)
 
 print("The value of the function at ",root," is ",val)
-
-print("The value of f\'(x) at -1 is",fprime(-1))
-print("The value of f\'(x) at -0.1 is",fprime(-0.1))
-
